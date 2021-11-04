8f1cbcd2f5d33d6389b968f6d1b9c1e32e18632b
Harbir Chahal <harbirchahal@hotmail.com>
2021-11-04 14:53:34 +0530

    file formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { DATA } from './data';
 import { Person } from './models';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
